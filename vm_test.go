8502abcb81e0772a7bbaa0e2c4eda0ffb2090d72
hmarui66 <hmarui66@gmail.com>
2021-10-08 20:04:41 +0900

    add HelloJVM file

--- vm_test.go
+++ vm_test.go
@@ -39,3 +39,36 @@ func TestVirtualMachine_ExecMain(t *testing.T) {
 	//		}},
 	//	}
 }
+
+func TestVirtualMachine_ExecMain_HelloJVM(t *testing.T) {
+	buf, err := ioutil.ReadFile("HelloJVM.class")
+	require.NoError(t, err)
+
+	class, err := DecodeClassStructure(bytes.NewBuffer(buf))
+	require.NoError(t, err)
+	vm := NewVM(class)
+
+	err = vm.ExecMain()
+	require.NoError(t, err)
+
+	//	c := CodeAttribute{
+	//		AttributeNameIndex:   9,
+	//		AttributeLength:      55,
+	//		MaxStack:             2,
+	//		MaxLocals:            1,
+	//		CodeLength:           9,
+	//		Code:                 []byte{178, 0, 2, 18, 3, 182, 0, 4, 177},
+	//		ExceptionTableLength: 0,
+	//		ExceptionTable:       nil,
+	//		AttributesCount:      2,
+	//		Attributes: []*AttributeInfo{{
+	//			AttributeNameIndex: 0xc0,
+	//			AttributeLength:    0x00,
+	//			Info:               []byte{0x15, 0x87, 0x80},
+	//		}, {
+	//			AttributeNameIndex: 0xc0,
+	//			AttributeLength:    0x00,
+	//			Info:               []byte{0x15, 0x87, 0xa0},
+	//		}},
+	//	}
+}
